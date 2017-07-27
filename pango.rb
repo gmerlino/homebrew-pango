57d01873edc3433f0cdd101da06f1c0ff0906984
Giovanni Merlino <gmerlino@unime.it>
2017-07-27 13:49:20 +0200

    Import

---
+++ pango.rb
@@ -0,0 +1,100 @@
+class Pango < Formula
+  desc "Framework for layout and rendering of i18n text"
+  homepage "http://www.pango.org/"
+  url "https://download.gnome.org/sources/pango/1.40/pango-1.40.7.tar.xz"
+  sha256 "517645c00c4554e82c0631e836659504d3fd3699c564c633fccfdfd37574e278"
+
+  bottle do
+    sha256 "56ed4949c060bc9f276fc23175a97e5ba34cb603118d92c7ef4c6b3876d6f188" => :sierra
+    sha256 "67bdaa908ae261446c384e9c80fdad3a861020219e328fcea77c2f5d4dfd5f98" => :el_capitan
+    sha256 "b310a0d7ea96190e911b772b1d80665ceed29bea3fe673d123beb078a2d57f7c" => :yosemite
+  end
+
+  head do
+    url "https://git.gnome.org/browse/pango.git"
+
+    depends_on "automake" => :build
+    depends_on "autoconf" => :build
+    depends_on "libtool" => :build
+    depends_on "gtk-doc" => :build
+  end
+
+  depends_on "pkg-config" => :build
+  depends_on "cairo"
+  depends_on "fontconfig"
+  depends_on :x11 => :optional
+  depends_on "glib"
+  depends_on "gobject-introspection"
+  depends_on "harfbuzz"
+
+  def install
+    args = %W[
+      --disable-dependency-tracking
+      --disable-silent-rules
+      --prefix=#{prefix}
+      --enable-man
+      --with-html-dir=#{share}/doc
+      --enable-introspection=yes
+      --enable-static
+    ]
+
+    if build.with? "x11"
+      args << "--with-xft"
+    else
+      args << "--without-xft"
+    end
+
+    system "./autogen.sh" if build.head?
+    system "./configure", *args
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/pango-view", "--version"
+    (testpath/"test.c").write <<-EOS.undent
+      #include <pango/pangocairo.h>
+
+      int main(int argc, char *argv[]) {
+        PangoFontMap *fontmap;
+        int n_families;
+        PangoFontFamily **families;
+        fontmap = pango_cairo_font_map_get_default();
+        pango_font_map_list_families (fontmap, &families, &n_families);
+        g_free(families);
+        return 0;
+      }
+    EOS
+    cairo = Formula["cairo"]
+    fontconfig = Formula["fontconfig"]
+    freetype = Formula["freetype"]
+    gettext = Formula["gettext"]
+    glib = Formula["glib"]
+    libpng = Formula["libpng"]
+    pixman = Formula["pixman"]
+    flags = %W[
+      -I#{cairo.opt_include}/cairo
+      -I#{fontconfig.opt_include}
+      -I#{freetype.opt_include}/freetype2
+      -I#{gettext.opt_include}
+      -I#{glib.opt_include}/glib-2.0
+      -I#{glib.opt_lib}/glib-2.0/include
+      -I#{include}/pango-1.0
+      -I#{libpng.opt_include}/libpng16
+      -I#{pixman.opt_include}/pixman-1
+      -D_REENTRANT
+      -L#{cairo.opt_lib}
+      -L#{gettext.opt_lib}
+      -L#{glib.opt_lib}
+      -L#{lib}
+      -lcairo
+      -lglib-2.0
+      -lgobject-2.0
+      -lintl
+      -lpango-1.0
+      -lpangocairo-1.0
+    ]
+    system ENV.cc, "test.c", "-o", "test", *flags
+    system "./test"
+  end
+end
