083ed8d94c724705044c14c23f1df8b5a537ff02
Giovanni Merlino <gmerlino@unime.it>
2018-10-18 17:07:03 +0200

    fixes

--- pango.rb
+++ pango.rb
@@ -20,13 +20,15 @@ class Pango < Formula
     depends_on "libtool" => :build
   end
 
+  option "with-x11"
+
   depends_on "gobject-introspection" => :build
   depends_on "pkg-config" => :build
   depends_on "fontconfig"
   depends_on "fribidi"
+  depends_on "glib"
   depends_on "gmerlino/cairo/cairo" => "with-x11"
   depends_on "gmerlino/harfbuzz/harfbuzz"
-  depends_on "glib"
   depends_on :x11 => :optional
 
   # This fixes a font-size problem in gtk
